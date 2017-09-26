8faa3c0dd56c38cfa54009adabe9ba0b3dacf29d
lamondlu <309728709@qq.com>
2017-09-27 06:57:24 +0800

    添加服务器启动项目和通用领域项目

---
+++ BookingLibrary.Domain.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookingLibrary.Domain.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ BookingLibrary.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookingLibrary.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
