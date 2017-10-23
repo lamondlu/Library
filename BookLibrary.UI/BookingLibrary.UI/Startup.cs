af3aac21ad76e14f7cad1df332dcb450fe2be210
lamondlu <309728709@qq.com>
2017-10-23 20:24:03 +0800

    添加网页端

---
+++ BookLibrary.UI/BookingLibrary.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookingLibrary.UI.Startup))]
+namespace BookingLibrary.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
