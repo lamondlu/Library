38f47bc4dfe252dcdf94553f1fb75a060bf1f090
lamondlu <309728709@qq.com>
2017-11-03 22:47:21 +0800

    修复脚本

--- DBScripts/BookRepositoryDB.sql
+++ DBScripts/BookRepositoryDB.sql
@@ -5,19 +5,68 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[BookRepository](
+CREATE TABLE [dbo].[Book](
 	[BookId] [uniqueidentifier] NOT NULL,
 	[BookName] [nvarchar](max) NOT NULL,
 	[ISBN] [nvarchar](max) NOT NULL,
 	[DateIssued] [datetime2](7) NOT NULL,
 	[Description] [nvarchar](max) NULL,
-	[Status] [int] NOT NULL,
  CONSTRAINT [PK_Book] PRIMARY KEY CLUSTERED 
 (
 	[BookId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
+GO
+/****** Object:  Table [dbo].[BookRepository]    Script Date: 11/3/2017 10:34:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[BookRepository](
+	[BookRepositoryId] [uniqueidentifier] NOT NULL,
+	[BookId] [uniqueidentifier] NOT NULL,
+	[Status] [int] NOT NULL,
+	[IsRemoved] [bit] NOT NULL,
+ CONSTRAINT [PK_BookRepository] PRIMARY KEY CLUSTERED 
+(
+	[BookRepositoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[History]    Script Date: 11/3/2017 10:34:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[History](
+	[HistoryId] [uniqueidentifier] NOT NULL,
+	[BookId] [uniqueidentifier] NOT NULL,
+	[BookRepositoryId] [uniqueidentifier] NOT NULL,
+	[Note] [nvarchar](max) NOT NULL,
+	[CreatedOn] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_History] PRIMARY KEY CLUSTERED 
+(
+	[HistoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[BookRepository]  WITH CHECK ADD  CONSTRAINT [FK_BookRepository_Book] FOREIGN KEY([BookId])
+REFERENCES [dbo].[Book] ([BookId])
+GO
+ALTER TABLE [dbo].[BookRepository] CHECK CONSTRAINT [FK_BookRepository_Book]
+GO
+ALTER TABLE [dbo].[History]  WITH CHECK ADD  CONSTRAINT [FK_History_BookRepository] FOREIGN KEY([BookRepositoryId])
+REFERENCES [dbo].[BookRepository] ([BookRepositoryId])
+GO
+ALTER TABLE [dbo].[History] CHECK CONSTRAINT [FK_History_BookRepository]
+GO
+ALTER TABLE [dbo].[History]  WITH CHECK ADD  CONSTRAINT [FK_History_History] FOREIGN KEY([BookId])
+REFERENCES [dbo].[Book] ([BookId])
+GO
+ALTER TABLE [dbo].[History] CHECK CONSTRAINT [FK_History_History]
 GO
 USE [master]
 GO
