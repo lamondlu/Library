bd92543a712e99c3793d5289686488e1317f596f
lamondlu <309728709@qq.com>
2017-11-20 20:44:42 +0800

    Add LibraryLogDB script

---
+++ DBScripts/LibraryLogDB.sql
@@ -0,0 +1,24 @@
+USE [LibraryLogDB]
+GO
+/****** Object:  Table [dbo].[Logs]    Script Date: 11/20/2017 8:43:44 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Logs](
+	[Id] [uniqueidentifier] NOT NULL,
+	[LogType] [int] NOT NULL,
+	[CommandName] [nvarchar](max) NULL,
+	[CommandUniqueId] [uniqueidentifier] NULL,
+	[EventName] [nvarchar](max) NULL,
+	[IsSuccess] [bit] NOT NULL,
+	[Message] [nvarchar](max) NULL,
+	[Data] [nvarchar](max) NULL,
+	[CreatedOn] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Logs] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
