4cd45cd4a411256a1e17633a8fd9e299c3d99134
Lamond Lu <lamond.lu@orioncloud.com.au>
2018-01-04 20:32:09 +0800

    Add Operation solution. I will add service discovery and health check server via consul and use the consul-template to update and reset the nginx server

---
+++ Library.Infrastructure.Operation.Core/IServiceDiscovery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Infrastructure.Operation.Core
+{
+    public interface IServiceDiscovery
+    {
+        void RegisterService();
+
+
+    }
+}
