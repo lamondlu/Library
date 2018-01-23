332d9a2eabf62a77566a9b460bfc59cb48f05415
Lamond Lu <lamond.lu@orioncloud.com.au>
2018-01-23 20:40:24 +0800

    Update the ocelot setting

--- Library.ApiGateway/Program.cs
+++ Library.ApiGateway/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Library.ApiGateway
 {
@@ -9,16 +13,28 @@ namespace Library.ApiGateway
 		public static void Main(string[] args)
 		{
 			IWebHostBuilder builder = new WebHostBuilder();
-			builder.ConfigureServices(s =>
-			{
+			builder.ConfigureServices(s => {
 				s.AddSingleton(builder);
 			});
-			builder.UseKestrel()
-				   .UseUrls("http://*:4999")
-				   .UseContentRoot(Directory.GetCurrentDirectory())
-				   .UseIISIntegration()
-				   .UseStartup<Startup>()
-				   .UseApplicationInsights();
+
+			builder
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.ConfigureAppConfiguration((hostingContext, config) =>
+				{
+					config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+					var env = hostingContext.HostingEnvironment;
+					config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+					config.AddJsonFile("configuration.json");
+					config.AddEnvironmentVariables();
+				})
+				.ConfigureLogging((hostingContext, logging) =>
+				{
+					logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+					logging.AddConsole();
+				})
+				.UseStartup<Startup>();
+
 			var host = builder.Build();
 			host.Run();
 		}
--- Library.ApiGateway/Startup.cs
+++ Library.ApiGateway/Startup.cs
@@ -11,40 +11,14 @@ namespace Library.ApiGateway
 {
 	public class Startup
 	{
-		public IConfigurationRoot Configuration { get; }
-
-		public Startup(IHostingEnvironment environment)
-		{
-			var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
-			builder.SetBasePath(environment.ContentRootPath)
-				   .AddJsonFile("appsettings.json", false, reloadOnChange: true)
-				   .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
-				   .AddEnvironmentVariables();
-
-			Configuration = builder.Build();
-		}
-
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddOcelot(Configuration);
+			services.AddOcelot();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		public void Configure(IApplicationBuilder app)
 		{
-
-			loggerFactory.AddConsole();
-			loggerFactory.AddDebug();
-
-
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.UseOcelot();
+			app.UseOcelot().Wait();
 		}
 	}
 }
