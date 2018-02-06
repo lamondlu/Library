4e1ccb80bc4c6985b879fd6bf7ccbb74cd33f3f7
Lamond Lu <lamond.lu@orioncloud.com.au>
2018-02-07 06:35:19 +0800

    check-in the miss file

---
+++ src/Library.Service.Identity/DTOs/CustomerDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Service.Identity.DTOs
+{
+	public class CustomerDTO
+	{
+		public string UserName { get; set; }
+
+		public string Password { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string MiddleName { get; set; }
+
+		public string LastName { get; set; }
+	}
+}
