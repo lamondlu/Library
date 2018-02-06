769955181ece487cc033a87e19614a9c46739e71
Lamond Lu <lunan198506@hotmail.com>
2018-02-06 06:24:40 -0800

    add missing file

---
+++ src/Library.Service.Identity.Domain/EventHandlers/UserCreatedEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using Library.Domain.Core;
+using Library.Domain.Core.DataAccessor;
+using Library.Domain.Core.Messaging;
+using Library.Service.Identity.Domain.DataAccessors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Service.Identity.Domain.EventHandlers
+{
+    public class UserCreatedEventHandler : BaseIdentityEventHandler<UserCreatedEvent>
+    {
+        public UserCreatedEventHandler(IIdentityReportDataAccessor reportDataAccessor, ICommandTracker commandTracker, ILogger logger, IDomainRepository domainRepository, IEventPublisher eventPublisher) : base(reportDataAccessor, commandTracker, logger, domainRepository, eventPublisher)
+        {
+
+        }
+
+        public override void HandleCore(UserCreatedEvent evt)
+        {
+            
+        }
+    }
+}
